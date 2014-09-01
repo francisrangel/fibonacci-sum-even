23e6259c526aeec189c5b4a33c93710e7bf1be08
Francis <francis.rangel@gmail.com>
2014-09-01 11:52:33 -0300

    Indenting

--- src/fibonacci_sum_even/core.clj
+++ src/fibonacci_sum_even/core.clj
@@ -2,11 +2,11 @@
 
 (def fib-seq
   ((fn rfib [a b]
-    (lazy-seq (cons a (rfib b (+ a b)))))
-  1 1))
+     (lazy-seq (cons a (rfib b (+ a b)))))
+   1 1))
 
 (defn fibonacci [n]
-	(take n fib-seq))
+  (take n fib-seq))
 
 (defn sum-even [n]
   (let [ sequence (fibonacci n)]
