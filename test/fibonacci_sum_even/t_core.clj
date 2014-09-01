7d2fa1058cb916725bc799fb230e4ad5fde12a15
Francis <francis.rangel@gmail.com>
2014-09-01 11:52:43 -0300

    Refactoring tests: using more examples instead of facts

--- test/fibonacci_sum_even/t_core.clj
+++ test/fibonacci_sum_even/t_core.clj
@@ -3,24 +3,14 @@
   (:use [fibonacci_sum_even.core]))
 
 (facts "about 'fibonacci sequence'"
-  (fact "it returns a sequence with the number 1 when asked for a sequence with one number"
-    (fibonacci 1) => '(1))
-
-  (fact "it returns a sequence with the number 1 twice when asked for a sequence with two numbers"
-    (fibonacci 2) => '(1 1))
-
-  (fact "it return a correct sequence with 8 elements"
+  (fact "it returns a fibonacci sequence"
+    (fibonacci 1) => '(1)
+    (fibonacci 2) => '(1 1)
     (fibonacci 8) => '(1 1 2 3 5 8 13 21)))
 
 (facts "about 'summing even numbers from fibonacci sequence'"
-  (fact "it returns 0 for a sequence with just one element"
-    (sum-even 1) => 0)
-
-  (fact "it returns 2 for a sequence with three elements"
-    (sum-even 3) => 2)
-
-  (fact "it returns 10 for a sequence with eight elements"
-    (sum-even 8) => 10)
-
-  (fact "it returns 44 for a sequence with ten elements"
+  (fact "it sums the even numbers on a fibonacci sequence"
+    (sum-even 1) => 0
+    (sum-even 2) => 0
+    (sum-even 3) => 2
     (sum-even 10) => 44))
