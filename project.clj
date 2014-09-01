971ca6b80b487a1aab2cbebac79b1be05f95238c
Francis Rangel <francis.rangel@gmail.com>
2014-09-01 01:07:13 -0300

    Adding lein-midje plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject fibonacci-sum-even "0.0.1-SNAPSHOT"
   :description "Project to learn clojure while summing even numbers from a Fibonacci sequence"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.1.1"]]}})
   
